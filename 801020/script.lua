bd46f0c5eeb24a4a0b26bfb45795d0116aff0063
valentinokascel1-cell <valentinokascel1@gmail.com>
2026-08-31 13:55:17 +0200

    Add lua script for App ID 801020

---
+++ 801020/script.lua
@@ -0,0 +1,7 @@
+-- This file was fetched from twentytwo cloud which is the exclusive property of Twentytwo server.
+-- Redistribution of twentytwo's files is not allowed AT ALL
+-- Join the server here: https://discord.com/invite/vwGWeTFTXW
+
+addappid(801020)
+addappid(801021,0,"d926f10d38807881a67882104480d957f682f7901c2d634ff6fef13114f16021")
+setManifestid(801021,"8206791185924434540")
