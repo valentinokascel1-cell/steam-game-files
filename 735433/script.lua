3e5951502785dfc302a56953c6985a04e246f367
valentinokascel1-cell <valentinokascel1@gmail.com>
2026-08-31 12:52:14 +0200

    Add lua script for App ID 735433

---
+++ 735433/script.lua
@@ -0,0 +1,7 @@
+-- This file was fetched from twentytwo cloud which is the exclusive property of Twentytwo server.
+-- Redistribution of twentytwo's files is not allowed AT ALL
+-- Join the server here: https://discord.com/invite/vwGWeTFTXW
+
+addappid(735433)
+addappid(735433,0,"d7e825f22e43dc161f08121f2b8f73398e0ac7f99768d58f5d94e9f85a2befd7")
+setManifestid(735433,"3137511021129445459")
