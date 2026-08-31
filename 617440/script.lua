60dabb2acd93d9fd2b544a03f77f2a11d2bb9a38
valentinokascel1-cell <valentinokascel1@gmail.com>
2026-08-31 11:12:28 +0200

    Add lua script for App ID 617440

---
+++ 617440/script.lua
@@ -0,0 +1,9 @@
+-- This file was fetched from twentytwo cloud which is the exclusive property of Twentytwo server.
+-- Redistribution of twentytwo's files is not allowed AT ALL
+-- Join the server here: https://discord.com/invite/vwGWeTFTXW
+
+addappid(617440)
+addappid(617441,0,"7360bdab98619976fcdd4ec41fa1ee4891a79939a67452d94384f897c4895420")
+setManifestid(617441,"6958692012210976327")
+addappid(684241)
+addappid(684290)
