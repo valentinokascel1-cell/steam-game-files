12002fe0d07c0169e30a318951a629f765a4570b
valentinokascel1-cell <valentinokascel1@gmail.com>
2026-08-31 07:10:06 +0200

    Add lua script for App ID 385470

---
+++ 385470/script.lua
@@ -0,0 +1,13 @@
+-- This file was fetched from twentytwo cloud which is the exclusive property of Twentytwo server.
+-- Redistribution of twentytwo's files is not allowed AT ALL
+-- Join the server here: https://discord.com/invite/vwGWeTFTXW
+
+addappid(385470)
+addappid(228990)
+setManifestid(228990,"1829726630299308803")
+addappid(385471,0,"3a2b20b82c390777ae022e0c32d8eb81d3d5ac59247a967268829b5c592c5a64")
+setManifestid(385471,"8123031267228880551")
+addappid(385472)
+addappid(385473)
+addappid(385474)
+addappid(385475)
