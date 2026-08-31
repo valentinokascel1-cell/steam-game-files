6bc8723f23db810529c7d3c664d3121f6510073d
valentinokascel1-cell <valentinokascel1@gmail.com>
2026-08-31 05:31:53 +0200

    Add lua script for App ID 298900

---
+++ 298900/script.lua
@@ -0,0 +1,31 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 298900 Manifest
+-- Name: Space Hulk: Deathwing
+-- Generated: 2025-06-07 06:54:45
+-- Total Depots: 7
+-- Total DLCs: 1
+-- Shared Depots: 4
+
+-- MAIN APPLICATION
+addappid(298900) -- Space Hulk: Deathwing
+
+-- MAIN APP DEPOTS
+addappid(298901, 1, "e4664380d87531974e5245e864dede7953cefc675e31e1852742a4479477ab95") -- Space Hulk: Deathwing Content
+--setManifestid(298901, "6097600481816586102", 0)
+addappid(298902, 1, "393bf82e73e0fbb213f9c3283866b3899594c12fba204f52b8bb07bf4812748a") -- SHDW_BinariesAndInisOnly
+--setManifestid(298902, "4598884261085734047", 0)
+addappid(298903, 1, "8bd1ca8c6f49a0b2bdc1f7a3aa320fc5f14fea39ccff6adca7580e9fc20accfb") -- Space Hulk: Deathwing Depot Redist
+--setManifestid(298903, "5605831346670607973", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+--setManifestid(228985, "3966345552745568756", 0)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+--setManifestid(228986, "8782296191957114623", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 0)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") -- .NET 4.0 Redist (Shared from App 228980)
+--setManifestid(229002, "7260605429366465749", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(523990) -- Space Hulk Deathwing - The Lost Mace of Corswain
