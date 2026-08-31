88e39f61b42fb1ecc2a065948102020a6d50288a
valentinokascel1-cell <valentinokascel1@gmail.com>
2026-08-31 13:03:12 +0200

    Add lua script for App ID 747910

---
+++ 747910/script.lua
@@ -0,0 +1,7 @@
+-- This file was fetched from twentytwo cloud which is the exclusive property of Twentytwo server.
+-- Redistribution of twentytwo's files is not allowed AT ALL
+-- Join the server here: https://discord.com/invite/vwGWeTFTXW
+
+addappid(747910)
+addappid(747911,0,"d11e026c4fea0e4a48a5529a94305dab00f148f1410c8a1bfc02da5b6064e4e7")
+setManifestid(747911,"3989138443756387342")
