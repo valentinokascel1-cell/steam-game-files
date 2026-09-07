3efff72fbc8326552d9510103e093e6173ba48fa
valentinokascel1-cell <valentinokascel1@gmail.com>
2026-09-08 01:58:40 +0200

    Upload 2878980.lua for App ID 2878980

---
+++ 2878980/2878980.lua
@@ -0,0 +1,64 @@
+-- 2878980's Lua and Manifest Created by Hubcap Manifest
+-- NBA 2K25
+-- Created: September 30, 2025 at 03:00:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 41
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2878980) -- NBA 2K25
+-- MAIN APP DEPOTS
+addappid(2878981, 1, "df09d8fa7b88da65f6fdce275ab09b85e5c12731cad10c147745c7501acb1f14") -- Depot 2878981
+setManifestid(2878981, "1749668799585948014", 151994725388)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- NBA 2K25 Gravity Ball (AppID: 3107390)
+addappid(3107390)
+addappid(3107390, 1, "a86b0e16a6d6f9946fc2671411ad8653123970931a5e289b242e3b1cf563381f") -- NBA 2K25 Gravity Ball - Depot 3107390
+setManifestid(3107390, "5851584071712106802", 1761902563)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2974820) -- NBA 2K25 Hall of Fame Pass Season 1
+addappid(2974830) -- NBA 2K25 - Season 1 Pro Pass
+addappid(2974840) -- NBA 2K25 Hall of Fame Pass Season 2
+addappid(2974850) -- NBA 2K25 Pro Pass Season 2
+addappid(2974860) -- NBA 2K25 Hall of Fame Pass Season 3
+addappid(2974870) -- NBA 2K25 Pro Pass Season 3
+addappid(2974880) -- NBA 2K25 Hall of Fame Pass Season 4
+addappid(2974890) -- NBA 2K25 Pro Pass Season 4
+addappid(2974900) -- NBA 2K25 Hall of Fame Pass Season 5
+addappid(2974910) -- NBA 2K25 Pro Pass Season 5
+addappid(2974920) -- NBA 2K25 Hall of Fame Pass Season 6
+addappid(2974930) -- NBA 2K25 Pro Pass Season 6
+addappid(2974940) -- NBA 2K25 Pro Pass Season 7
+addappid(2974950) -- NBA 2K25 Pro Pass Season 8
+addappid(2974960) -- NBA 2K25 Pro Pass Season 9
+addappid(2974970) -- NBA 2K25 Hall of Fame Pass Season 7
+addappid(2974980) -- NBA 2K25 Hall of Fame Pass Season 8
+addappid(2974990) -- NBA 2K25 Hall of Fame Pass Season 9
+addappid(2975000) -- NBA 2K25 5,000 Virtual Currency Pack
+addappid(2975010) -- NBA 2K25 Tournament Edition Bonus
+addappid(2975020) -- NBA 2K25 All-Star Bonus
+addappid(2982770) -- NBA 2K25 Hall of Fame Bonus
+addappid(2982780) -- VC MyCAREER Bundle Season 1
+addappid(2982790) -- VC MyTEAM Bundle Season 1
+addappid(3261610) -- NBA 2K25 MyTEAM Bonus Offer Season 2
+addappid(3261620) -- NBA 2K25 MyCAREER Bonus Offer Season 2
+addappid(3310490) -- NBA 2K25 MyTEAM Bonus Offer Season 3
+addappid(3310500) -- NBA 2K25 MyCAREER Bonus Offer Season 3
+addappid(3312950) -- NBA 2K25 MyTEAM Bonus Offer Season 4
+addappid(3312960) -- NBA 2K25 MyCAREER Bonus Offer Season 4
+addappid(3312970) -- NBA 2K25 MyTEAM Bonus Offer Season 5
+addappid(3312980) -- NBA 2K25 MyCAREER Bonus Offer Season 5
+addappid(3312990) -- NBA 2K25 MyTEAM Bonus Offer Season 6
+addappid(3313000) -- NBA 2K25 MyCAREER Bonus Offer Season 6
+addappid(3313010) -- NBA 2K25 MyTEAM Bonus Offer Season 7
+addappid(3313020) -- NBA 2K25 MyCAREER Bonus Offer Season 7
+addappid(3313030) -- NBA 2K25 MyTEAM Bonus Offer Season 8
+addappid(3313040) -- NBA 2K25 MyCAREER Bonus Offer Season 8
+addappid(3313050) -- NBA 2K25 MyTEAM Bonus Offer Season 9
+addappid(3313060) -- NBA 2K25 MyCAREER Bonus Offer Season 9
